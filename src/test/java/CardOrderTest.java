b7a6579c2645bdc1d31ddf693a1da7e39c6b5ae2
Alena Bobrova <kaisa8721@gmail.com>
2023-05-24 21:17:14 +0300

    Tests fifth commit

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -2,7 +2,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import com.codeborne.selenide.SelenideElement;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -10,21 +10,22 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.files.DownloadActions.click;
 import static org.openqa.selenium.Keys.BACK_SPACE;
 
 public class CardOrderTest {
-    String generateDate (int daysToAdd, String pattern){
+    String generateDate(int daysToAdd, String pattern) {
         return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern(pattern));
     }
-    String generateDateDay (int daysToAdd, String pattern){
+
+    String generateDateDay(int daysToAdd, String pattern) {
         return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern(pattern));
     }
+
     @Test
     void shouldRegister() {
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("Казань");
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id= 'date'] input").setValue(generateDate(3, "dd.MM.yyyy")).sendKeys(Keys.chord(Keys.ESCAPE));
         $("[data-test-id='name'] input").setValue("Иванова Анна");
         $("[data-test-id= 'phone'] input").setValue("+79261111111");
@@ -33,11 +34,12 @@ public class CardOrderTest {
         $("[data-test-id= 'notification']").shouldBe(appear, Duration.ofSeconds(15));
 
     }
+
     @Test
     void latinCity() {
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("Kazan");
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id= 'date'] input").setValue(generateDate(3, "dd.MM.yyyy")).sendKeys(Keys.chord(Keys.ESCAPE));
         $("[data-test-id='name'] input").setValue("Иванова Анна");
         $("[data-test-id= 'phone'] input").setValue("+79261111111");
@@ -50,7 +52,7 @@ public class CardOrderTest {
     void emptyCity() {
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("");
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id= 'date'] input").setValue(generateDate(3, "dd.MM.yyyy")).sendKeys(Keys.chord(Keys.ESCAPE));
         $("[data-test-id='name'] input").setValue("Иванова Анна");
         $("[data-test-id= 'phone'] input").setValue("+79261111111");
@@ -58,11 +60,12 @@ public class CardOrderTest {
         $$("button").find(exactText("Забронировать")).click();
         $x("//span[@data-test-id='city']//span[contains(text(), 'Поле обязательно для заполнения')]").should(appear);
     }
+
     @Test
     void latinName() {
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("Казань");
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id= 'date'] input").setValue(generateDate(3, "dd.MM.yyyy")).sendKeys(Keys.chord(Keys.ESCAPE));
         $("[data-test-id='name'] input").setValue("Ivanova Anna");
         $("[data-test-id= 'phone'] input").setValue("+79261111111");
@@ -70,11 +73,12 @@ public class CardOrderTest {
         $$("button").find(exactText("Забронировать")).click();
         $x("//span[@data-test-id='name']//span[contains(text(), 'Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.')]").should(appear);
     }
+
     @Test
     void emptyName() {
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("Казань");
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id= 'date'] input").setValue(generateDate(3, "dd.MM.yyyy")).sendKeys(Keys.chord(Keys.ESCAPE));
         $("[data-test-id='name'] input").setValue("");
         $("[data-test-id= 'phone'] input").setValue("+79261111111");
@@ -82,11 +86,12 @@ public class CardOrderTest {
         $$("button").find(exactText("Забронировать")).click();
         $x("//span[@data-test-id='name']//span[contains(text(), 'Поле обязательно для заполнения')]").should(appear);
     }
+
     @Test
     void spaceName() {
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("Казань");
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id= 'date'] input").setValue(generateDate(3, "dd.MM.yyyy")).sendKeys(Keys.chord(Keys.ESCAPE));
         $("[data-test-id='name'] input").setValue(" ");
         $("[data-test-id= 'phone'] input").setValue("+79261111111");
@@ -94,11 +99,12 @@ public class CardOrderTest {
         $$("button").find(exactText("Забронировать")).click();
         $x("//span[@data-test-id='name']//span[contains(text(), 'Поле обязательно для заполнения')]").should(appear);
     }
+
     @Test
     void emptyPhone() {
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("Казань");
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id= 'date'] input").setValue(generateDate(3, "dd.MM.yyyy")).sendKeys(Keys.chord(Keys.ESCAPE));
         $("[data-test-id='name'] input").setValue("Иванова Анна");
         $("[data-test-id= 'phone'] input").setValue("");
@@ -106,11 +112,12 @@ public class CardOrderTest {
         $$("button").find(exactText("Забронировать")).click();
         $x("//span[@data-test-id='phone']//span[contains(text(), 'Поле обязательно для заполнения')]").should(appear);
     }
+
     @Test
     void shortPhone() {
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("Казань");
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id= 'date'] input").setValue(generateDate(3, "dd.MM.yyyy")).sendKeys(Keys.chord(Keys.ESCAPE));
         $("[data-test-id='name'] input").setValue("Иванова Анна");
         $("[data-test-id= 'phone'] input").setValue("+792611111");
@@ -118,34 +125,37 @@ public class CardOrderTest {
         $$("button").find(exactText("Забронировать")).click();
         $x("//span[@data-test-id='phone']//span[contains(text(), 'Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.')]").should(appear);
     }
+
     @Test
     void unmarkedCheckBox() {
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("Казань");
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id= 'date'] input").setValue(generateDate(3, "dd.MM.yyyy")).sendKeys(Keys.chord(Keys.ESCAPE));
         $("[data-test-id='name'] input").setValue("Иванова Анна");
         $("[data-test-id= 'phone'] input").setValue("+79261111111");
         $$("button").find(exactText("Забронировать")).click();
         $x("//label[@data-test-id='agreement'][contains(@class, 'input_invalid')]").should(appear);
     }
+
     @Test
     void emptyDate() {
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("Казань");
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id='name'] input").setValue("Иванова Анна");
         $("[data-test-id= 'phone'] input").setValue("+79261111111");
         $("[data-test-id= 'agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
         $x("//span[@data-test-id='date']//span[contains(text(), 'Неверно введена дата')]").should(appear);
     }
+
     @Test
     void shouldChooseCity() {
         open("http://localhost:9999");
         $("[data-test-id= 'city'] input").setValue("Ка");
         $(withText("Калуга")).click();
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id= 'date'] input").setValue(generateDate(3, "dd.MM.yyyy")).sendKeys(Keys.chord(Keys.ESCAPE));
         $("[data-test-id='name'] input").setValue("Иванова Анна");
         $("[data-test-id= 'phone'] input").setValue("+79261111111");
@@ -153,11 +163,12 @@ public class CardOrderTest {
         $$("button").find(exactText("Забронировать")).click();
         $("[data-test-id= 'notification']").shouldBe(appear, Duration.ofSeconds(15));
     }
+
     @Test
     void shouldDataThisMonth() {
         open("http://localhost:9999");
         $("[data-test-id = 'city'] input").setValue("Казань");
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id = 'date'] input").click();
         String date = generateDateDay(3, "dd");
         $(withText(date)).click();
@@ -167,11 +178,40 @@ public class CardOrderTest {
         $$("button").find(exactText("Забронировать")).click();
         $("[data-test-id= 'notification']").shouldBe(appear, Duration.ofSeconds(15));
     }
+
+    @Test
+    void shouldDataThisMonthLast() {
+        open("http://localhost:9999");
+        $("[data-test-id = 'city'] input").setValue("Казань");
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").click();
+        $$("td[data-day]").last().click();
+        $("[data-test-id='name'] input").setValue("Иванова Анна");
+        $("[data-test-id= 'phone'] input").setValue("+79261111111");
+        $("[data-test-id= 'agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id= 'notification']").shouldBe(appear, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldDataThisMonthFirst() {
+        open("http://localhost:9999");
+        $("[data-test-id = 'city'] input").setValue("Казань");
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").click();
+        $$("td[data-day]").first().click();
+        $("[data-test-id='name'] input").setValue("Иванова Анна");
+        $("[data-test-id= 'phone'] input").setValue("+79261111111");
+        $("[data-test-id= 'agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id= 'notification']").shouldBe(appear, Duration.ofSeconds(15));
+    }
+
     @Test
     void shouldDataNextMonth() {
         open("http://localhost:9999");
         $("[data-test-id = 'city'] input").setValue("Казань");
-        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL,"a"), BACK_SPACE);
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), BACK_SPACE);
         $("[data-test-id = 'date'] input").click();
         $("[class= 'calendar__arrow calendar__arrow_direction_right']").click();
         $(byText("5")).click();
@@ -182,3 +222,4 @@ public class CardOrderTest {
         $("[data-test-id= 'notification']").shouldBe(appear, Duration.ofSeconds(15));
     }
 }
+
